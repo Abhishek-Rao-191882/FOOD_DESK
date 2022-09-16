32c5b6d48a52f43f137727b18e163d4eab778767
Abhishek Rao <arao8732@gmail.com>
2022-09-16 21:21:41 +0530

    env added

--- frontend/seller_frontend/src/API/API.js
+++ frontend/seller_frontend/src/API/API.js
@@ -17,49 +17,49 @@ export default {
 
     UserInfo(){
         const token = localStorage.getItem('token')
-        return axios.post('http://localhost:8000/accounts/sellerlogin',{"token":token})
+        return axios.post(process.env.VUE_APP_SERVER+'/accounts/sellerlogin',{"token":token})
     },
 
     logout(){
         const token = localStorage.getItem('token')
-        return axios.post('http://localhost:8000/accounts/logout',{"token":token})
+        return axios.post(process.env.VUE_APP_SERVER+'/accounts/logout',{"token":token})
     },
 
     getProducts(id){
         // const id = user_id
-        return axios.get(`http://localhost:8000/seller/products/${id}`)
+        return axios.get(`${process.env.VUE_APP_SERVER}/seller/products/${id}`)
     },
 
     EditProducts(product_id,formData2){
-        return axios.put(`http://localhost:8000/seller/products/${product_id}`,formData2)
+        return axios.put(`${process.env.VUE_APP_SERVER}/products/${product_id}`,formData2)
     },
 
     DeleteProducts(data){
-        return axios.delete(`http://localhost:8000/seller/products/${data.id}`)
+        return axios.delete(`${process.env.VUE_APP_SERVER}/seller/products/${data.id}`)
     },
 
     getCategories(id){
-        return axios.post('http://localhost:8000/seller/categories',{"userId":id})
+        return axios.post(process.env.VUE_APP_SERVER+'/seller/categories',{"userId":id})
     },
 
     AddCategories(id,category_name){
-        return axios.post('http://localhost:8000/seller/categoriesOperations/',{'Seller':id,'Category_Name':category_name})
+        return axios.post(process.env.VUE_APP_SERVER+'/seller/categoriesOperations/',{'Seller':id,'Category_Name':category_name})
     },
 
     EditCategories(id,category_id,category_name){
-        return axios.put(`http://localhost:8000/seller/categoriesOperations/${category_id}/`,{'Seller':id,'Category_Name':category_name})
+        return axios.put(`${process.env.VUE_APP_SERVER}/seller/categoriesOperations/${category_id}/`,{'Seller':id,'Category_Name':category_name})
     },
 
     DeleteCategories(id){
-        return axios.delete(`http://localhost:8000/seller/categoriesOperations/${id}`)
+        return axios.delete(`${process.env.VUE_APP_SERVER}/seller/categoriesOperations/${id}`)
     },
 
     getOrders(id,type){
-        return axios.get(`http://localhost:8000/orders/seller_get_orders/${id}/${type}`)
+        return axios.get(`${process.env.VUE_APP_SERVER}/orders/seller_get_orders/${id}/${type}`)
     },
     
     acceptOrder(id,user_id,acceptance_status){
-        return axios.post("http://localhost:8000/orders/seller_get_orders/",{'id':id,'Seller_id':user_id,'Acceptance_Status':acceptance_status})
+        return axios.post(process.env.VUE_APP_SERVER+'/orders/seller_get_orders/',{'id':id,'Seller_id':user_id,'Acceptance_Status':acceptance_status})
     }
     // login(data,token)
     // {
